b054776f1941aa21cf988490e220c628419767be
Voltis <v.agolli@live.com>
2019-01-03 12:41:23 +0100

    show list only on sign in, redirect on sign out

--- src/components/schedules/ScheduleList.js
+++ src/components/schedules/ScheduleList.js
@@ -5,25 +5,23 @@ import { fetchSchedules } from '../../actions';
 
 class ScheduleList extends React.Component {
     componentDidMount() {
-        this.props.fetchSchedules(this.props.currentUserId);
+        // this.props.fetchSchedules(this.props.currentUserId);
     }
 
-    renderAdmin(schedule) {
-        if (this.props.currentUserId) {
-            return (
-                <div className="right floated content">
-                    <Link to={'/schedules/edit/' + schedule.Id} className="ui button primary">Edit</Link>
-                    <Link to={'/schedules/delete/' + schedule.Id} className="ui button negative">Delete</Link>
-                </div>
-            );
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.schedules.length === 0 && this.props.currentUserId && this.props.schedules.length === 0) {
+            this.props.fetchSchedules(this.props.currentUserId);
         }
     }
 
     renderList() {
-        return this.props.schedules.map(schedule => {
+        const renderList = this.props.schedules.map(schedule => {
             return (
                 <div className="item" key={schedule.Id}>
-                    {this.renderAdmin(schedule)}
+                    <div className="right floated content">
+                        <Link to={'/schedules/edit/' + schedule.Id} className="ui button primary">Edit</Link>
+                        <Link to={'/schedules/delete/' + schedule.Id} className="ui button negative">Delete</Link>
+                    </div>
                     <i className="large middle aligned icon video" />
                     <div className="content">
                         <Link to={'/schedules/' + schedule.Id} className="header">
@@ -36,39 +34,45 @@ class ScheduleList extends React.Component {
                 </div>
             );
         });
+
+        return renderList;
     }
 
-    renderCreate() {
-        if (this.props.isSignedIn) {
+    renderAdmin() {
+        if (this.props.isSignedIn && this.props.currentUserId) {
             return (
-                <div style={{ textAlign: 'right' }}>
-                    <Link to="/schedules/new" className="ui button primary">
-                        Create schedule
-                    </Link>
+                <div>
+                    <h2>Schedules</h2>
+                    <div style={{ textAlign: 'right' }}>
+                        <Link to="/schedules/new" className="ui button primary">
+                            Create schedule
+                        </Link>
+                    </div>
+                    <div className="ui celled list">
+                        {this.renderList()}
+                    </div>
                 </div>
+            );
+        } else {
+            return (
+                <h3>Please Log In</h3>
             );
         }
     }
 
     render() {
-        return (
-            <div>
-                <h2>Schedules</h2>
-                <div className="ui celled list">
-                    {this.renderList()}
-                </div>
-                {this.renderCreate()}
-            </div>
-        );
+        return (this.renderAdmin());
     }
 }
 
 const mapStateToProps = (state) => {
+
     return {
         schedules: Object.values(state.schedules),
         currentUserId: state.auth.userId,
         isSignedIn: state.auth.isSignedIn
     };
+
 }
 
 export default connect(mapStateToProps, { fetchSchedules })(ScheduleList);
